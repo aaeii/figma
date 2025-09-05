7581088f11f0fe5f097e3acc951ff66b0e19fbda
aaeii <ahm-emily@yandex.ru>
2025-09-05 19:10:45 +0300

    fix all

--- src/js/calendar.js
+++ src/js/calendar.js
@@ -9,12 +9,14 @@ var Cal = function(divId) {
     'Сб',
     'Вс'
   ];
-  this.Months =['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
+  this.Months = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
   
   var d = new Date();
   this.currMonth = d.getMonth();
   this.currYear = d.getFullYear();
   this.currDay = d.getDate();
+
+  
 };
 
 Cal.prototype.nextMonth = function() {
@@ -45,58 +47,58 @@ Cal.prototype.showcurr = function() {
 };
 
 Cal.prototype.showMonth = function(y, m) {
-  var d = new Date()
-  , firstDayOfMonth = new Date(y, m, 7).getDay()
-  , lastDateOfMonth =  new Date(y, m+1, 0).getDate()
-  , lastDayOfLastMonth = m == 0 ? new Date(y-1, 11, 0).getDate() : new Date(y, m, 0).getDate();
+  var d = new Date(),
+      firstDayOfMonth = new Date(y, m, 7).getDay(),
+      lastDateOfMonth = new Date(y, m + 1, 0).getDate(),
+      lastDayOfLastMonth = m == 0 ? new Date(y - 1, 11, 0).getDate() : new Date(y, m, 0).getDate();  
+      
   var html = '<table>';
   html += '<thead><tr>';
   html += '<td colspan="7">' + this.Months[m] + ' ' + y + '</td>';
   html += '</tr></thead>';
+  
   html += '<tr class="days">';
-  for(var i=0; i < this.DaysOfWeek.length;i++) {
-    html += '<td>' + this.DaysOfWeek[i] + '</td>';
+  for(var i = 0; i < this.DaysOfWeek.length; i++) {
+    html += '<td class="week">' + this.DaysOfWeek[i] + '</td>';
   }
   html += '</tr>';
-  var i=1;
+  
+  var i = 1;
   do {
     var dow = new Date(y, m, i).getDay();
-    if ( dow == 1 ) {
+  
+    if (dow == 1) {
       html += '<tr>';
-    }
-    else if ( i == 1 ) {
+    } else if (i == 1) {
       html += '<tr>';
-      var k = lastDayOfLastMonth - firstDayOfMonth+1;
-      for(var j=0; j < firstDayOfMonth; j++) {
+      var k = lastDayOfLastMonth - firstDayOfMonth + 1;
+      for (var j = 0; j < firstDayOfMonth; j++) {
         html += '<td class="not-current">' + k + '</td>';
         k++;
       }
     }
-    var chk = new Date();
-    var chkY = chk.getFullYear();
-    var chkM = chk.getMonth();
-    if (chkY == this.currYear && chkM == this.currMonth && i == this.currDay) {
-      html += '<td class="today">' + i + '</td>';
-    } else {
-      html += '<td class="normal">' + i + '</td>';
-    }
-    if ( dow == 0 ) {
+
+    var targetDateClass = 'normal';
+    html += `<td class="${targetDateClass}" data-date="${i}" onclick="selectDate(event, ${i})">${i}</td>`;
+
+    if (dow == 0) {
       html += '</tr>';
-    }
-    else if ( i == lastDateOfMonth ) {
-      var k=1;
-      for(dow; dow < 7; dow++) {
+    } else if (i == lastDateOfMonth) {
+      var k = 1;
+      for (dow; dow < 7; dow++) {
         html += '<td class="not-current">' + k + '</td>';
         k++;
       }
     }
     i++;
-  }while(i <= lastDateOfMonth);
+  } while (i <= lastDateOfMonth);
+  
   html += '</table>';
   document.getElementById(this.divId).innerHTML = html;
 };
+
 window.onload = function() {
-  var c = new Cal("divCal");			
+  var c = new Cal("divCal");      
   c.showcurr();
   getId('btnNext').onclick = function() {
     c.nextMonth();
@@ -105,6 +107,21 @@ window.onload = function() {
     c.previousMonth();
   };
 }
+
 function getId(id) {
   return document.getElementById(id);
+}
+
+function selectDate(event, day) {
+  const normalCells = document.querySelectorAll('.normal, .today');
+  normalCells.forEach(cell => {
+    cell.classList.remove('selected');
+  });
+
+  const target = event.target;
+  target.classList.add('selected');
+  const dateContainer = document.getElementById("selectedDate");
+  dateContainer.innerHTML = `${day} ${new Cal().Months[new Cal().currMonth]}`;
+// dateContainer.innerHTML = `${day ? ' ' + day : ''} ${new Cal().Months[new Cal().currMonth]}`;
+
 }
