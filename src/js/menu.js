4fb3730c7bd898f540fcff01d8dfbd8d582aff6d
aaeii <ahm-emily@yandex.ru>
2025-09-06 17:52:48 +0300

    add blur

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,5 +1,9 @@
 const burgerMenu = document.getElementById("burgerMenu");
 const menu = document.querySelector("nav.menu");
+const mainContent = document.querySelector("main");
+
 burgerMenu.addEventListener("click", function() {
-    menu.classList.toggle("active"); 
+    menu.classList.toggle("active");
+    mainContent.classList.toggle("blur");
 });
+
